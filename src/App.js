cca3f04c1fb66287179b87676186d2c769c71108
Muhammad Sameem <sameemgujjar123@yahoo.com>
2022-09-29 03:25:26 +0400

    :white_check_mark: Form validation done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "./index.css";
 import ColorRenderer from "./01-color-renderer/ColorRenderer";
 import DarkMode from "./02-dark-mode/DarkMode";
+import FormValidator from "./03-form-validator/FormValidator";
 
 function App() {
   return (
     <div className="App">
       {/* <ColorRenderer /> */}
-      <DarkMode />
+      {/* <DarkMode /> */}
+      <FormValidator />
     </div>
   );
 }
